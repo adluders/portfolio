313a979984d7f4b6f5725a48aab06f036a7d1a17
Adler Luders <adlerluders@gmail.com>
2020-08-02 17:59:11 -0400

    finalized basic layout of application

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import React from "react";
 import "./css/App.css";
 import { Route, Switch } from "react-router-dom";
 
+import ProjectContext from "./context/ProjectContext";
+
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
+
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ProjectContext from "./context/ProjectContext";
-import Contact from "./pages/Contact";
-import { useState } from "react";
-import { useEffect } from "react";
-import MobileNav from "./components/MobileNav";
 
 const App = () => {
-  const [isMobile, setMobile] = useState(false);
-
-  useEffect(() => {
-    window.innerWidth < 500 && setMobile(true);
-  }, []);
-
   return (
     <ProjectContext>
-      {isMobile ? <MobileNav /> : <Navbar />}
+      <Navbar />
+      {/*isMobile ? <MobileNav /> : <Navbar />*/}
       <div className="container">
         <Switch>
           <Route exact path="/" component={Home} />
